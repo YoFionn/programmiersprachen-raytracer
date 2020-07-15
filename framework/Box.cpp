3c44d307676576d46ad4c374526b2d42c85f9a42
YoFionn <ericksoncooperation@gmail.com>
2020-07-15 16:20:11 +0200

    Aufgabe 3, die erweitertung von Konstruktoren

--- framework/Box.cpp
+++ framework/Box.cpp
@@ -3,10 +3,17 @@
 #include <iostream>
 
 Box::Box() :
+	Shape(),
 	min_{0.0f, 0.0f, 0.0f},
 	max_{ 1.0f, 1.0f, 1.0f }{}
 
 Box::Box(glm::vec3 min, glm::vec3 max) :
+	Shape(),
+	min_{ min },
+	max_{ max }{}
+
+Box::Box(glm::vec3 min, glm::vec3 max, std::string name, Color color):
+	Shape(name, color),
 	min_{ min },
 	max_{ max }{}
 
